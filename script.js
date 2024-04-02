e72ae8af094dbe766cef900218142d15d6770681
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-04-02 11:52:13 +0530

    Username and password changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loginAdmin(){
     let username = document.getElementById("inputUserName").value;
     let password = document.getElementById("inputPassword").value;
 
-    if(username == "1" && password == "2"){
+    if(username == "admin@Pmsa" && password == "webtool@Pmsa"){
         window.location.href = "admin-post2.html";
     }else{
         alert("Invalid username or password");
